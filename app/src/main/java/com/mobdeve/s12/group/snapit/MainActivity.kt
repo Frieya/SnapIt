ab94fda26e458db77658327284e63754021fa1c6
stanpai78 <stanpai78@gmail.com>
2023-12-02 17:03:13 +0800

    Remove unused imports

--- app/src/main/java/com/mobdeve/s12/group/snapit/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/group/snapit/MainActivity.kt
@@ -1,16 +1,7 @@
 package com.mobdeve.s12.group.snapit
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mobdeve.s12.group.snapit.databinding.ActivityMainmenuBinding
@@ -31,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         this.imageAdapter = ImageAdapter(this.image_data)
         this.recyclerView.adapter = imageAdapter
 
-        var layoutManager = GridLayoutManager(this, 2)
+        val layoutManager = GridLayoutManager(this, 2)
         this.recyclerView.layoutManager = layoutManager
 
     }
