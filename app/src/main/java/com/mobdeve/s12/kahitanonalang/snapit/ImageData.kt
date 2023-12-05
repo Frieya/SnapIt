3c68b20af6253e70559c7843987d9323d958fae8
stanpai78 <stanpai78@gmail.com>
2023-12-05 19:32:16 +0800

    Add ImageData keys

--- app/src/main/java/com/mobdeve/s12/kahitanonalang/snapit/ImageData.kt
+++ app/src/main/java/com/mobdeve/s12/kahitanonalang/snapit/ImageData.kt
@@ -5,4 +5,13 @@ import java.util.Date
 
 class ImageData(var title: String, var imgUrl: Int,
                 var latitude: Float, var longtitude: Float,
-                var takenDate: Date, var description: String)
+                var takenDate: Date, var description: String) {
+    companion object {
+        val keyTitle = "TITLE_KEY"
+        val keyImgUrl = "URL_KEY"
+        val keyLatitude = "LAT_KEY"
+        val keyLongitude = "LONG_KEY"
+        val keyDate = "DATE_KEY"
+        val keyDescription = "DESC_KEY"
+    }
+}
